4b9cec6c73ba645fdf4e7923587caa7510e1a160
Marco Antonio Molina López <marco.molina@segip.gob.bo>
2020-11-30 08:36:12 -0400

    Organizacion de imports

--- src/app/base/base.module.ts
+++ src/app/base/base.module.ts
@@ -1,18 +1,16 @@
-import { ProgressBarComponent } from './progress-bar/progress-bar.component';
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
+import { baseRoutes } from './base.routing';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
 import { LayoutComponent } from './layout/layout.component';
 import { LoginComponent } from './login/login.component';
 import { MenuComponent } from './menu/menu.component';
-import { NotificacionComponent } from './notificacion/notificacion.component';
 import { NotfoundComponent } from './notfound/notfound.component';
-import { SharedModule } from '../shared/shared.module';
-import { AuthGuardService } from '../shared/services/auth-guard.service';
+import { NotificacionComponent } from './notificacion/notificacion.component';
+import { ProgressBarComponent } from './progress-bar/progress-bar.component';
 import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
-import { ErrorViewerComponent } from '../shared/components/error-viewer/error-viewer.component';
-import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
-import { baseRoutes } from './base.routing';
 
 /**
  * Modulo base del sistema.
