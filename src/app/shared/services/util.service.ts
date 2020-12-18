927839c8d5e9ef79ac3883a6fba4da414db9c08c
Marco Antonio Molina López <marco.molina@segip.gob.bo>
2020-12-18 10:37:26 -0400

    Creacion del metodo Util : createDocumentFromBlob()

--- src/app/shared/services/util.service.ts
+++ src/app/shared/services/util.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
+import { eTipoArchivo } from '../enums/tipo-archivo.enum';
 /**
  * Servicio que provee metodos utilitarios.
  *
@@ -15,5 +16,25 @@ export class UtilService {
      */
     getFechaActual(): Date {
         return new Date();
+    }
+
+    /**
+     * Crea un documento de tipo 'pTipoArchivo' a partir de un blob o buffer.
+     *
+     * @param {Blob} pBlob
+     * @param {string} [pTpoArchivo] Tipo de archivo, si no se especifica se asigna tipo WORD @see eTipoArchivo
+     * @param {string} [pNnombreArchivo] Nombre del archivo, si no se especifica, se toma la hora del sistema
+     * @memberof UtilService
+     */
+    createDocumentFromBlob( pBlob: Blob, pTpoArchivo?: string, pNnombreArchivo?: string): void {
+      const tipoArchivo = pTpoArchivo ? pTpoArchivo : eTipoArchivo.Word;
+      const nombreArchivo = pNnombreArchivo ? pNnombreArchivo : `archivo-${new Date().getTime()}`;
+
+      const blob = new Blob([<any> pBlob], {type: `${tipoArchivo}`});
+      const url = window.URL.createObjectURL(blob);
+      const anchor = document.createElement('a');
+      anchor.download = `${nombreArchivo}`;
+      anchor.href = url;
+      anchor.click();
     }
 }
