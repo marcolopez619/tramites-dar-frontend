5171cbacd1afb56fd958ce6b3567676509579ce8
SANTOS_QUISPE_APAZA <santos.quispe@segip.gob.bo>
2020-12-31 13:17:26 -0400

    modificacion del componente derivar

--- src/app/hoja-de-ruta/components/derivar/derivar.component.ts
+++ src/app/hoja-de-ruta/components/derivar/derivar.component.ts
@@ -6,16 +6,20 @@ import {
   Validators,
 } from "@angular/forms";
 import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { MatTableDataSource } from "@angular/material/table";
 import { takeUntil } from "rxjs/internal/operators/takeUntil";
+import { CitesService } from "../../../cites/cites.service";
+import { CiteModelByUsuario } from "../../../cites/models/cites.models";
 import {
   fadeInAnim,
   slideInLeftAnim,
 } from "../../../shared/animations/template.animation";
 import { BaseComponent } from "../../../shared/base.component";
 import { ComentarioModel } from "../../../shared/models/comentario.model";
-import { UsuarioModel } from "../../../shared/models/Usuario.model";
+import { DestinatarioModel, UsuarioModel } from "../../../shared/models/Usuario.model";
 import { ComentarioService } from "../../../shared/services/comentario.service";
 import { ContextoService } from "../../../shared/services/contexto.service";
+import { DocumentoAdjuntoService } from "../../../shared/services/documento-adjunto.service";
 import { LangService } from "../../../shared/services/lang.service";
 import { UsuarioService } from "../../../shared/services/usuario.service";
 import { DerivarModel } from "../../models/derivar.model";
@@ -28,15 +32,15 @@ import { DerivarModel } from "../../models/derivar.model";
 })
 export class DerivarComponent extends BaseComponent implements OnInit {
   listaUsuarios: Array<UsuarioModel> = [];
+  listaCite: Array<CiteModelByUsuario> = [];
+
+
+
   longMaxDescripcion = 500;
   formDerivarHR: FormGroup;
   private _isDestinatarioInvalid: boolean;
   listaDestinatarios: Array<UsuarioModel> = [];
-  listaCite: Array<DerivarModel> = [
-    { IdCite: 1, DescripcionCite: 'SEGIP/NDAF/INF/011-2020' },
-    { IdCite: 2, DescripcionCite: 'SEGIP/NDAF/XXX/012-2020' },
-    { IdCite: 3, DescripcionCite: 'SEGIP/NDAF/YYY/013-2020' }
-  ];
+  dataSource = new MatTableDataSource<CiteModelByUsuario>([]);
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
@@ -44,18 +48,18 @@ export class DerivarComponent extends BaseComponent implements OnInit {
     public contextService: ContextoService,
     public langService: LangService,
     private formBuilder: FormBuilder,
-    private usuarioService: UsuarioService
-  ) //private comentarioService: ComentarioService
+    private usuarioService: UsuarioService,
+    private citesService: CitesService,
+    private documentoAdjuntoService: DocumentoAdjuntoService
+  )
   {
     super();
   }
 
   ngOnInit(): void {
-    /*
-    const listaFuncionario: Array<string> = [ 'SARDINA GUMUCIO FLORIPONDIO', 'CONDORI GUMER TITO', 'JUAN JOSE PEREZ CASTILLO' ];
-    */
-    const listaCite: Array<string> = [ 'SEGIP/NDAF/INF/011-2020', 'SEGIP/NDAF/DOC/012-2020', 'SEGIP/NDAF/XXX/013-2020' ];
 
+    const idPersonaGd = this.contextService.getItemContexto(`idPersonaGd`) ?? 542;
+    this.getAllCitesFromPersona( idPersonaGd )
     const dataForm: DerivarModel = {};
 
     // Carga los usuarios de la bd
@@ -64,9 +68,7 @@ export class DerivarComponent extends BaseComponent implements OnInit {
 
     this.formDerivarHR = this.formBuilder.group({
       listaDestinatarios: [undefined, Validators.compose([Validators.required])],
-      listaCite: [ 'SEGIP/NDAF/INF/011-2020', 'SEGIP/NDAF/DOC/012-2020', 'SEGIP/NDAF/XXX/013-2020' ]
     });
-
   }
 
   private getAllusuarios(idTipotramite: number): void {
@@ -99,13 +101,16 @@ export class DerivarComponent extends BaseComponent implements OnInit {
     );
   }
 
-  save(): void {
+  private getAllCitesFromPersona( idPersonaGd: number ): void {
+    this.citesService.getAllCitesFromPersona( idPersonaGd ).pipe( takeUntil( this.unsubscribe$ ) ).subscribe( listaCitesPersona => {
+      this.listaCite = listaCitesPersona.data as Array<CiteModelByUsuario>;
+    });
+  }
 
+  save(): void {
+    this.documentoAdjuntoService.sendFlagToSaveDocument(true);
     const datosFormulario: DerivarModel = { };
-    //datosFormulario.idDerivacion = 1;
-    // TODO: Logica para guardar la derivacion de la hoja de ruta.
-    //this.comentarioService.insertComentario(datosFormulario).pipe(takeUntil(this.unsubscribe$)).subscribe(result => {
-    //});
+
   }
 
   cancelar(): void {
