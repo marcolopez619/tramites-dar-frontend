7e391ba1d36c7153aeeee822c862dd744b9647c2
Marco Antonio Molina López <marco.molina@segip.gob.bo>
2021-01-13 16:06:17 -0400

    Implementacion del metodo: onCiteSeleccionChange, para mostrar la referencia del cite seleccionado en el componente shared hoja-ruta.

--- src/app/shared/components/hoja-de-ruta/hoja-de-ruta.component.ts
+++ src/app/shared/components/hoja-de-ruta/hoja-de-ruta.component.ts
@@ -22,6 +22,7 @@ import { ParametricaService } from '../../services/parametrica.service';
 import { UsuarioService } from '../../services/usuario.service';
 import { CiteModelByUsuario } from '../../../cites/models/cites.models';
 import { CitesService } from '../../../cites/cites.service';
+import { AutocompleteData } from '../../models/autocomplete.model';
 
 export class Test {
 
@@ -64,15 +65,12 @@ export class HojaDeRutaComponent extends BaseComponent implements OnInit {
     private parametricaService: ParametricaService,
     private usuarioService: UsuarioService,
     private hojaRutaService: HojaRutaService,
-    private citesService: CitesService,
+    private citesService: CitesService
   ) {
     super();
   }
 
   ngOnInit(): void {
-    const idTipoTramiteDefault = 1;
-    setTimeout(()=>this.data = new Test(),0)
-
     this.citeSelected = this.data.citeSelected as CiteModelByUsuario;
 
     this.parametricaService
@@ -111,13 +109,62 @@ export class HojaDeRutaComponent extends BaseComponent implements OnInit {
         listaCc           : [undefined],
         numeroCite        : [undefined, Validators.compose([Validators.required])],
         referencia        : [undefined, Validators.compose([Validators.required])],
-        numeroFojas       : [undefined, Validators.compose([Validators.required])],
-        plazoDias         : [undefined, Validators.compose([Validators.required])],
+        numeroFojas       : [ 0, Validators.compose([Validators.required])],
+        plazoDias         : [ 0, Validators.compose([Validators.required])],
         listaCite         : [undefined, Validators.compose([Validators.required])],
         isUrgente         : [false, Validators.compose([Validators.required])],
         isConCopiaFisica  : [false, Validators.compose([Validators.required])]
       });
     }
+  }
+
+
+
+  private prepareArrayAsJSONString( lista: Array<any>, nombreKey: string ): Array<any> {
+    const listaJSON = [];
+    const listaIdPersona = lista.map((cc) => cc.idPersonaGd);
+
+    listaIdPersona.forEach((id) => {
+      if (nombreKey === 'idPersonaGd') {
+        listaJSON.push({ idPersonaGd: id });
+      }
+    });
+
+    return listaJSON;
+  }
+
+  private getAllCitesFromPersona( idPersonaGd: number ): void {
+    this.citesService.getAllCitesFromPersona( idPersonaGd ).pipe( takeUntil( this.unsubscribe$ ) ).subscribe( listaCitesPersona => {
+      this.listaCite = listaCitesPersona.data as Array<CiteModelByUsuario>;
+    });
+  }
+
+  private getAllusuarios(idTipotramite: number, isChangeFromTramiteChange?: boolean): void {
+    // Borra los datos de las listas
+    this.listaDestinatarios.length = this.listaRemitentes.length = this.listaUsuarios.length = 0;
+
+    this.usuarioService
+      .getAllUsuarios(idTipotramite)
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe((respService) => {
+        this.listaUsuarios = respService.data;
+
+        if ( isChangeFromTramiteChange ) {
+          this.listaInicialRemitentes = [];
+          this.listaInicialRemitentes.length = 0;
+        } else {
+          // Recupera la data del usuario loggeado como remitente.
+          const idPersonaGDLoggeado = this.contextService.getItemContexto('idPersonaGd');
+          this.setListaInicialRemitente(idPersonaGDLoggeado);
+        }
+
+      });
+    }
+
+  private setListaInicialRemitente( idPersonaGD: number ): void {
+    const usuario = this.listaUsuarios.find( x => x.idPersonaGd === idPersonaGD );
+    this.listaInicialRemitentes.push(usuario);
+    this.formHojaDeRuta.controls['listaRemitentes'].setValue(usuario);
   }
 
   onGuardarHojaDeRuta(): void {
@@ -179,75 +226,9 @@ export class HojaDeRutaComponent extends BaseComponent implements OnInit {
       });
   }
 
-  selectionChangeCite(objEventCite: any): void {
-    var algo=1;
-    const objUbiGeo: CiteModelByUsuario = {
-        idCite: objEventCite.value
-    };
-    const objUbiCiud: UsuarioModel = {
-      idPersonaGd: objEventCite.value
-    };
-
-    /*
-    this.idPaisSelect = Number(objUbiGeo.idPais);
-    this.gabineteService.getDepartamento(objUbiGeo.idPais)
-    .pipe(takeUntil(this.unsubscribe$))
-    .subscribe((response) => {
-        this.comboDepartamento = response.data;
-    }, (err) => {
-        // if (err.status === 404) {
-        //     this.comboDepartamento = [];
-        //     this.formFuncionarioCargo.get('idFuncion').markAsTouched({ onlySelf: true });
-        // }
-    });*/
-
-  }
-
-  private prepareArrayAsJSONString( lista: Array<any>, nombreKey: string ): Array<any> {
-    const listaJSON = [];
-    const listaIdPersona = lista.map((cc) => cc.idPersonaGd);
-
-    listaIdPersona.forEach((id) => {
-      if (nombreKey === 'idPersonaGd') {
-        listaJSON.push({ idPersonaGd: id });
-      }
-    });
-
-    return listaJSON;
-  }
-
-  private getAllCitesFromPersona( idPersonaGd: number ): void {
-    this.citesService.getAllCitesFromPersona( idPersonaGd ).pipe( takeUntil( this.unsubscribe$ ) ).subscribe( listaCitesPersona => {
-      this.listaCite = listaCitesPersona.data as Array<CiteModelByUsuario>;
-    });
-  }
-
-  private getAllusuarios(idTipotramite: number, isChangeFromTramiteChange?: boolean): void {
-    // Borra los datos de las listas
-    this.listaDestinatarios.length = this.listaRemitentes.length = this.listaUsuarios.length = 0;
-
-    this.usuarioService
-      .getAllUsuarios(idTipotramite)
-      .pipe(takeUntil(this.unsubscribe$))
-      .subscribe((respService) => {
-        this.listaUsuarios = respService.data;
-
-        if ( isChangeFromTramiteChange ) {
-          this.listaInicialRemitentes = [];
-          this.listaInicialRemitentes.length = 0;
-        } else {
-          // Recupera la data del usuario loggeado como remitente.
-          const idPersonaGDLoggeado = this.contextService.getItemContexto('idPersonaGd');
-          this.setListaInicialRemitente(idPersonaGDLoggeado);
-        }
-
-      });
-    }
-
-  private setListaInicialRemitente( idPersonaGD: number ): void {
-    const usuario = this.listaUsuarios.find( x => x.idPersonaGd === idPersonaGD );
-    this.listaInicialRemitentes.push(usuario);
-    this.formHojaDeRuta.controls['listaRemitentes'].setValue(usuario);
+  onCiteSeleccionChange(event: MatSelectChange): void {
+    const infCiteSelected = this.listaCite.find( x => x.idCite === event.value );
+    this.formHojaDeRuta.controls['referencia'].setValue(infCiteSelected.referencia);
   }
 
   onTipoTramiteChange(event: MatSelectChange): void {
@@ -277,40 +258,23 @@ export class HojaDeRutaComponent extends BaseComponent implements OnInit {
     console.log(' is Invalid Remitente : ' + $event);
   }
 
-  getListaSeleccionadaRemitentes($event): void {
-    console.log('----------------------');
-    this.listaRemitentes = $event as Array<UsuarioModel>;
-
-    this.listaRemitentes.forEach((element) => {
-      console.log('Remitente ---> ' + element.nombreCompleto);
-    });
-    this.formHojaDeRuta.controls['listaRemitentes'].setValue(
-      this._isRemitenteInvalid ? undefined : this.listaRemitentes
-    );
-    const idPersonaGd = this.listaRemitentes[0].idPersonaGd
+  getListaSeleccionadaRemitentes(event: AutocompleteData): void {
+    this.listaRemitentes = event.listaSeleccionados;
+    this.formHojaDeRuta.controls['listaRemitentes'].setValue( this._isRemitenteInvalid ? undefined : this.listaRemitentes );
+    const idPersonaGd = this.listaRemitentes[0].idPersonaGd;
     this.getAllCitesFromPersona( idPersonaGd );
   }
 
-  getListaSeleccionadaDestinatarios($event): void {
-    console.log('----------------------');
-    this.listaDestinatarios = $event as Array<UsuarioModel>;
-    this.listaDestinatarios.forEach((element) => {
-      console.log(' Destinatario ---> ' + element.nombreCompleto);
-    });
+  getListaSeleccionadaDestinatarios(event: AutocompleteData): void {
+    this.listaDestinatarios = event.listaSeleccionados;
     this.formHojaDeRuta.controls['listaDestinatarios'].setValue(
       this._isDestinatarioInvalid ? undefined : this.listaDestinatarios
     );
   }
 
-  getListaSeleccionadaCc($event): void {
-    console.log('----------------------');
-    this.listaCc = $event as Array<UsuarioModel>;
-    this.listaCc.forEach((element) => {
-      console.log(' Destinatario ---> ' + element.nombreCompleto);
-    });
-    this.formHojaDeRuta.controls['listaCc'].setValue(
-      this._isCcInvalid ? undefined : this.listaCc
-    );
+  getListaSeleccionadaCc(event: AutocompleteData): void {
+    this.listaCc = event.listaSeleccionados;
+    this.formHojaDeRuta.controls['listaCc'].setValue( this._isCcInvalid ? undefined : this.listaCc );
   }
 
   onClose(object?: any): void {
