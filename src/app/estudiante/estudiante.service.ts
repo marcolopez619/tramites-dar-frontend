08d6d29433ead6f663b466e4b83e3e36e4a04e79
Marco Antonio Molina López <marco.molina@segip.gob.bo>
2021-05-02 18:30:55 -0400

    Creacion del servicio de obtencion de informacion del estudiante.

--- src/app/estudiante/estudiante.service.ts
+++ src/app/estudiante/estudiante.service.ts
@@ -1,7 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Resultado } from '../shared/models/resultado.model';
+import { ContextoService } from '../shared/services/contexto.service';
 
 @Injectable()
 export class EstudianteService {
 
-  constructor() { }
+  readonly baseURL = this.contextoService.getConfig(`backendApi`).concat('/estudiante');
+
+  constructor(
+    private httpClient: HttpClient,
+    private contextoService: ContextoService
+  ) { }
+
+  getInformacionEstudiante(pIdEstudiante: number): Observable<Resultado> {
+    return this.httpClient.get<Resultado>(`${this.baseURL}/${pIdEstudiante}`);
+  }
+
+
 }
--- src/app/shared/models/estudiante.model.ts
+++ src/app/shared/models/estudiante.model.ts
@@ -1,5 +1,29 @@
-export interface EstudianteModel{
+export interface EstudianteModel {
+  idFacultad?: number;
+  facultad?: string;
+  idCarrera?: number;
+  carrera?: string;
+  idEstudiante?: number;
   ru?: number;
+  ci?: string;
+  complemento?: string;
+  paterno?: string;
+  materno?: string;
+  nombres?: string;
+  nombreCompleto?: string;
+  fechaNacimiento?: Date;
+  sexo?: boolean;
+  fotografia?: string;
+
+  // Lo de abajo se utiliza en otros modulos, asi q ver la posibilidad de obtenerlo en la consulta de la inf del estudiante
+  fechaSolicitud?: Date;
+  tipoTramite?: string;
+  anioIngreso?: number;
+  cantMateriasAprobadas?: number;
+  cantMateriasReprobadas?: number;
+  promedioGeneral?: number;
+
+  /* ru?: number;
   ci?: string;
   nombreCompleto?: string;
   fotografia?: string;
@@ -12,5 +36,5 @@ export interface EstudianteModel{
   anioIngreso?: number;
   cantMateriasAprobadas?: number;
   cantMateriasReprobadas?: number;
-  promedioGeneral?: number;
+  promedioGeneral?: number; */
 }
